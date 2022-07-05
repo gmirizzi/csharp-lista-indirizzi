a135bb00646a8aeceb5c53d380e0b354d0eed752
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-05 14:29:13 +0200

    Aperto il file

--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -1,4 +1,13 @@
-﻿public class Indirizzo
+﻿StreamReader file = File.OpenText("C:\\Users\\Giuseppe Mirizzi\\source\\repos\\csharp-lista-indirizzi\\ListaIndirizzi\\addresses.csv");
+
+while (!file.EndOfStream)
+{
+    string riga=file.ReadLine();
+    Console.WriteLine(riga);
+}
+
+file.Close();
+public class Indirizzo
 {
     private string street;
     private string city;
