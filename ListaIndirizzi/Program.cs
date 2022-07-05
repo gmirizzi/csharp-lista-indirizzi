4041ef1b375a49f6a7490d7a66f77a1d507d2522
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-05 15:08:00 +0200

    ciclo per popolare la lista di indirizzi

--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -3,10 +3,23 @@
 //Aperutra file con indirizzi
 StreamReader file = File.OpenText("C:\\Users\\Giuseppe Mirizzi\\source\\repos\\csharp-lista-indirizzi\\ListaIndirizzi\\addresses.csv");
 
+string intestazione = file.ReadLine();
+
 while (!file.EndOfStream)
-{
-    string riga=file.ReadLine();
-    Console.WriteLine(riga);
+{   
+    try
+    {
+        string riga=file.ReadLine();
+        string[] arrData = riga.Split(",");
+        Indirizzo address = new Indirizzo(arrData[2], arrData[3]);
+        address.SetProvince(arrData[4]);
+        address.SetZip(arrData[5]);
+        addressesList.Add(address);
+        Console.WriteLine(riga);
+    } catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
 }
 
 file.Close();
@@ -15,11 +28,21 @@ public class Indirizzo
     private string street;
     private string city;
     private string province;
-    private string zipCode;
+    private int zipCode;
+
+    public void SetProvince (string province)
+    {
+        this.province = province;
+    }
+    public void SetZip(string zip)
+    {
+        this.zipCode = int.Parse(zip);
+    }
 
     public Indirizzo(string street, string city)
-    {
-        this.street = street;
+    {   
+        if (street!="")
+            this.street = street;
         this.city = city;
     }
 }
