8ac2093c9bcd4412b98b337c975f61c9e5b98e29
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-05 14:17:22 +0200

    creata la classe indirizzo

---
+++ ListaIndirizzi/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Indirizzo
+{
+    private string street;
+    private string city;
+    private string province;
+    private string zipCode;
+
+    public Indirizzo(string street, string city)
+    {
+        this.street = street;
+        this.city = city;
+    }
+}
